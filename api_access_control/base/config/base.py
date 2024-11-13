089accd472f068ed4c7eaa51dfe7bcd81e558bc4
ronaldjdev <eronald2009@gmail.com>
2024-11-12 20:10:30 -0500

    Agregamos visualizacion en buffer de los qr

--- api_access_control/base/config/base.py
+++ api_access_control/base/config/base.py
@@ -131,8 +131,8 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "user.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
