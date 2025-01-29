11dd637b96d674b39a156260c2e6fc90b1981aa9
ronaldjdev <eronald2009@gmail.com>
2025-01-28 23:57:40 -0500

    📊 feat: add option to group and simplify overtime before export
    🔹 Implemented a feature to consolidate and simplify overtime calculations before exporting data.
    🔹 Enhances clarity in exported reports by reducing redundant entries.

--- api_access_control/register_access/admin.py
+++ api_access_control/register_access/admin.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.contrib import admin
 from import_export import resources
 from tablib import Dataset
@@ -7,6 +8,7 @@ from .models import QrCode, RegisterAccess
 import holidays
 from datetime import datetime
 from rangefilter.filters import DateRangeFilter
+from collections import defaultdict
 
 # class RegisterAccessResource(resources.ModelResource):
 #     user = Field(attribute="user", column_name="user", readonly=True)
@@ -19,7 +21,16 @@ colombia_holidays = holidays.Colombia(years=datetime.now().year)
 
 
 def is_sunday_or_holiday(date):
-    # Verificar si la fecha es un domingo o un festivo
+    """
+    Determines if the given date is either a Sunday or a public holiday.
+
+    Args:
+        date (datetime): The date to be checked.
+
+    Returns:
+        bool: True if the date is a Sunday or a holiday in Colombia, False otherwise.
+    """
+
     return date.weekday() == 6 or date in colombia_holidays
 
 
@@ -42,7 +53,22 @@ class ExportRegisterAccessResource(resources.ModelResource):
         fields = ()
 
     def dehydrate_concepto(self, obj):
-        # Verificar si es domingo o festivo
+        """
+        Asigna el concepto correspondiente a las horas extras trabajadas
+        teniendo en cuenta si es domingo o festivo.
+
+        Si es domingo o festivo, se asigna el concepto "DV10" para las horas
+        extras diurnas y "DV11" para las horas extras nocturnas.
+
+        Si no es domingo o festivo, se asigna el concepto "DV08" para las horas
+        extras diurnas y "DV09" para las horas extras nocturnas.
+
+        Args:
+            obj (RegisterAccess): el objeto de registro de acceso
+
+        Returns:
+            str: el concepto correspondiente a las horas extras trabajadas
+        """
         if is_sunday_or_holiday(obj.user_entry):
             if obj.extra_hours > 0:
                 return "DV10"  # Horas extras dominicales y festivas
@@ -57,7 +83,11 @@ class ExportRegisterAccessResource(resources.ModelResource):
         return ""
 
     def dehydrate_cantidad(self, obj):
-        # Asignar la cantidad correspondiente al concepto
+        """
+        Asigna la cantidad correspondiente al concepto de las horas extras diurnas o nocturnas,
+        o 0 si no hay horas extras. Se utiliza para exportar la cantidad correspondiente al
+        concepto en el archivo de exportación.
+        """
         if obj.extra_hours > 0:
             return obj.extra_hours
         elif obj.extra_hours_night > 0:
@@ -66,6 +96,16 @@ class ExportRegisterAccessResource(resources.ModelResource):
         return 0
 
     def get_export_headers(self):
+        """
+        Devuelve los encabezados para la exportación de registros de acceso.
+
+        Los encabezados incluyen información sobre el contrato, nombre del usuario,
+        concepto de horas extras trabajadas, nombre calculado, y cantidad de horas.
+
+        Returns:
+            list: Una lista de cadenas que representan los encabezados de las columnas.
+        """
+
         return ["Contrato", "Nombre", "Concepto", "C_Nombre", "Cantidad"]
 
     def export(self, queryset=None, *args, **kwargs):
@@ -86,66 +126,32 @@ class ExportRegisterAccessResource(resources.ModelResource):
             "Nombre", "Concepto", "C_nombre", "Cantidad".
         """
         dataset = Dataset(headers=self.get_export_headers())
-        if queryset.exists():
+
+        if queryset is None or not queryset.exists():
             queryset = self._meta.model.objects.all()
-            print("queryset: ",queryset)
 
+        # Diccionario para agrupar datos antes de exportarlos
+        grouped_data = defaultdict(Decimal)
 
-        # Iterar sobre cada registro de acceso
         for obj in queryset:
-            # Datos base para todas las filas de este registro
             contrato = obj.user.id_card
             nombre = f"{obj.user.name.upper()} {obj.user.last_name.upper()}"
 
-            # Generar filas según las horas extras
-            # Primero verificamos si es domingo o festivo
+            # Determinar los conceptos según si es domingo o festivo
             if is_sunday_or_holiday(obj.user_entry):
-                # Si es domingo o festivo, asignamos los conceptos correspondientes
                 if obj.extra_hours > 0:
-                    dataset.append(
-                        [
-                            contrato,
-                            nombre,
-                            "DV10",
-                            "DV10",
-                            obj.extra_hours,
-                        ]
-                    )
-
+                    grouped_data[(contrato, nombre, "DV10", "DV10")] += obj.extra_hours
                 if obj.extra_hours_night > 0:
-                    dataset.append(
-                        [
-                            contrato,
-                            nombre,
-                            "DV11",
-                            "DV11",
-                            obj.extra_hours_night,
-                        ]
-                    )
+                    grouped_data[(contrato, nombre, "DV11", "DV11")] += obj.extra_hours_night
             else:
-                # Si no es domingo ni festivo, asignamos los conceptos para horas extras diurnas y nocturnas
                 if obj.extra_hours > 0:
-                    dataset.append(
-                        [
-                            contrato,
-                            nombre,
-                            "DV08",
-                            "DV08",
-                            obj.extra_hours,
-                        ]
-                    )
-
+                    grouped_data[(contrato, nombre, "DV08", "DV08")] += obj.extra_hours
                 if obj.extra_hours_night > 0:
-                    dataset.append(
-                        [
-                            contrato,
-                            nombre,
-                            "DV09",
-                            "DV09",
-                            obj.extra_hours_night,
-                        ]
-                    )
-            # Agregar concepto para horas extras dominicales y festivas
+                    grouped_data[(contrato, nombre, "DV09", "DV09")] += obj.extra_hours_night
+
+        # Agregar los datos agrupados al dataset
+        for (contrato, nombre, concepto, c_nombre), cantidad in grouped_data.items():
+            dataset.append([contrato, nombre, concepto, c_nombre, round(cantidad, 2)])
 
         return dataset
 
